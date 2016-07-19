9b7df79e6741a2451dbad7249284d7cb00bf7370
Cesar Velarde <juvega@gmail.com>
2016-07-18 21:59:29 -0500

    Uso de extension con paquetes nugets

--- WebDeveloper/Areas/Person/Controllers/PersonController.cs
+++ WebDeveloper/Areas/Person/Controllers/PersonController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using WebDeveloper.DataAccess;
 using WebDeveloper.Models;
+using WebDeveloper.Utils;
 
 namespace WebDeveloper.Areas.Person.Controllers
 {
@@ -24,7 +25,7 @@ namespace WebDeveloper.Areas.Person.Controllers
         }
 
         public PartialViewResult EmailList(int? id)
-        {
+        {            
             if(!id.HasValue) return null;
             return PartialView("_EmailList",_personRepository.EmailList(id.Value));
         }
